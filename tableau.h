a0b065ec537f7a55a3526b891d8a3d14d02b0044
QimingZheng <qimingzheng96@gmail.com>
2024-03-20 22:49:31 +0800

    Simplex tableau

---
+++ tableau.h
@@ -0,0 +1,261 @@
+#pragma once
+
+#include <assert.h>
+
+#include <algorithm>
+
+typedef int64_t tableau_index_t;
+typedef int64_t tableau_size_t;
+
+template <typename T>
+class Tableau;
+
+/* A list is the abstraction of a row or column in a simplex tableau. */
+template <typename T>
+class List {
+ public:
+  class Iterator {
+   public:
+    Iterator(List<T>* list) : list_(list) { index_ = 0; }
+
+    tableau_index_t Index() { return list_->index_[index_]; }
+    T Data() { return list_->data_[index_]; }
+
+    Iterator* Next() {
+      index_ += 1;
+      return this;
+    }
+
+    bool IsEnd() { return index_ >= list_->size_; }
+
+   private:
+    List<T>* list_;
+    tableau_index_t index_;
+  };
+
+  Iterator* Begin() { return new Iterator(this); }
+
+  List(tableau_size_t size = 0) {
+    capacity_ = 1;
+    size_ = 0;
+    while (capacity_ < size) {
+      capacity_ *= 2;
+    }
+    index_ = new tableau_index_t[capacity_];
+    data_ = new T[capacity_];
+  }
+  ~List() {
+    if (capacity_ > 0) {
+      delete data_;
+      delete index_;
+    }
+    data_ = nullptr;
+    index_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+  }
+
+  List(const List<T>* other) {
+    size_ = other->size_;
+    capacity_ = other->capacity_;
+    index_ = new tableau_index_t[capacity_];
+    data_ = new T[capacity_];
+    std::memcpy(index_, other->index_, sizeof(tableau_index_t) * capacity_);
+    std::memcpy(data_, other->data_, sizeof(T) * capacity_);
+  }
+
+  T At(tableau_index_t index) {
+    tableau_index_t lower = 0, upper = size_ - 1;
+    while (lower <= upper) {
+      tableau_index_t middle = (lower + upper) / 2;
+      if (index_[middle] == index) {
+        return data_[middle];
+      }
+      if (index_[middle] > index) {
+        upper = middle - 1;
+      } else {
+        lower = middle + 1;
+      }
+    }
+    return 0;
+  }
+
+  void Add(const List<T>* other) {
+    capacity_ = Size() + other->Size();
+    tableau_index_t* merged_index = new tableau_index_t[Size() + other->Size()];
+    T* merged_data = new T[Size() + other->Size()];
+    tableau_index_t left_index = 0, right_index = 0, next_index = 0;
+
+    while (left_index < Size() && right_index < other->Size()) {
+      if (index_[left_index] == other->index_[right_index]) {
+        merged_data[next_index] = data_[left_index] + other->data_[right_index];
+        merged_index[next_index] = index_[left_index];
+        left_index++;
+        right_index++;
+      } else if (index_[left_index] < other->index_[right_index]) {
+        merged_data[next_index] = data_[left_index];
+        merged_index[next_index] = index_[left_index];
+        left_index++;
+      } else {
+        merged_data[next_index] = other->data_[right_index];
+        merged_index[next_index] = other->index_[right_index];
+        right_index++;
+      }
+      next_index++;
+    }
+    while (left_index < other->Size()) {
+      merged_data[next_index] = data_[left_index];
+      merged_index[next_index] = index_[left_index];
+      next_index++;
+      left_index++;
+    }
+    while (right_index < other->Size()) {
+      merged_data[next_index] = other->data_[right_index];
+      merged_index[next_index] = other->index_[right_index];
+      next_index++;
+      right_index++;
+    }
+    size_ = next_index;
+    delete index_;
+    delete data_;
+    index_ = merged_index;
+    data_ = merged_data;
+  }
+
+  void Scale(const T scale) {
+    for (tableau_index_t i = 0; i < size_; i++) data_[i] *= scale;
+  }
+
+  T Dot(const List<T>* other) const {
+    tableau_index_t left = 0, right = 0;
+    T product = 0;
+    while (left < Size() and right < other->Size()) {
+      if (index_[left] == other->index_[right]) {
+        product += data_[left] * other->data_[right];
+        left += 1;
+        right += 1;
+      } else if (index_[left] < other->index_[right])
+        left += 1;
+      else
+        right += 1;
+    }
+    return product;
+  }
+
+  template <typename R>
+  List<R>* Map(R (*transform)(const T&)) const {
+    List<R>* list = new List<R>(Size());
+    for (tableau_index_t i = 0; i < Size(); i++) {
+      list->Append(index_[i], transform(data_[i]));
+    }
+    return list;
+  }
+
+  typedef std::pair<tableau_index_t, T> ReduceStruct;
+
+  ReduceStruct Reduce(ReduceStruct (*reduce)(const ReduceStruct&,
+                                             const ReduceStruct&),
+                      ReduceStruct initial_value) const {
+    for (tableau_index_t i = 0; i < Size(); i++) {
+      initial_value = reduce({index_[i], data_[i]}, initial_value);
+    }
+    return initial_value;
+  }
+
+  Tableau<T>* Cross(const List<T>* other, tableau_size_t rows,
+                    tableau_size_t cols) const;
+
+  tableau_size_t Size() const { return size_; }
+
+  void Append(tableau_index_t index, T value) {
+    if (size_ >= capacity_) {
+      capacity_ *= 2;
+      T* new_data = new T[capacity_];
+      tableau_index_t* new_index = new tableau_index_t[capacity_];
+
+      std::memcpy(new_index, index_, sizeof(tableau_index_t) * capacity_);
+      std::memcpy(new_data, data_, sizeof(T) * capacity_);
+
+      delete index_;
+      delete data_;
+      index_ = new_index;
+      data_ = new_data;
+    }
+    index_[size_] = index;
+    data_[size_] = value;
+    size_ += 1;
+  }
+
+  friend class Iterator;
+
+ private:
+  tableau_size_t size_ = 0;
+  tableau_size_t capacity_ = 0;
+  tableau_index_t* index_ = nullptr;
+  T* data_ = nullptr;
+};
+
+/* A Simplex Tableau. */
+template <typename T>
+class Tableau {
+ public:
+  Tableau(tableau_size_t rows, tableau_size_t columns)
+      : rows_(rows), columns_(columns) {
+    row_heads_ = new List<T>*[rows];
+    for (tableau_size_t i = 0; i < rows; i++) row_heads_[i] = new List<T>();
+    col_heads_ = new List<T>*[columns];
+    for (tableau_size_t i = 0; i < columns; i++) col_heads_[i] = new List<T>();
+  }
+  T At(tableau_index_t row, tableau_index_t col) { return Row(row)->At(col); }
+
+  List<T>* Row(tableau_index_t row) const { return row_heads_[row]; }
+  List<T>* Col(tableau_index_t col) const { return col_heads_[col]; }
+
+  void SetRow(tableau_index_t row, List<T>* list) {
+    delete row_heads_[row];
+    row_heads_[row] = list;
+  }
+  void SetCol(tableau_index_t col, List<T>* list) {
+    delete col_heads_[col];
+    col_heads_[col] = list;
+  }
+
+  void Add(const Tableau<T>* other) {
+    assert(rows_ == other->rows_);
+    assert(columns_ == other->columns_);
+
+    for (tableau_index_t row = 0; row < rows_; row++) {
+      Row(row)->Add(other->Row(row));
+    }
+
+    for (tableau_index_t col = 0; col < columns_; col++) {
+      Col(col)->Add(other->Col(col));
+    }
+  }
+
+ private:
+  tableau_size_t rows_, columns_;
+  List<T>** row_heads_;
+  List<T>** col_heads_;
+};
+
+template <typename T>
+Tableau<T>* List<T>::Cross(const List<T>* other, tableau_size_t rows,
+                           tableau_size_t cols) const {
+  Tableau<T>* tableau = new Tableau<T>(rows, cols);
+  for (tableau_index_t i = 0; i < Size(); i++) {
+    List<T>* row = new List<T>(other);
+    tableau_index_t index = index_[i];
+    T scale = data_[i];
+    row->Scale(scale);
+    tableau->SetRow(index, row);
+  }
+  for (tableau_index_t i = 0; i < other->Size(); i++) {
+    List<T>* col = new List<T>(other);
+    tableau_index_t index = other->index_[i];
+    T scale = other->data_[i];
+    col->Scale(scale);
+    tableau->SetCol(index, col);
+  }
+  return tableau;
+}
---
+++ tableau_test.cc
@@ -0,0 +1,116 @@
+#include "tableau.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+typedef float T;
+
+TEST(List, Append) {
+  List<T> list;
+  for (auto i = 0; i < 1024; i += 1) {
+    EXPECT_EQ(list.At(i), 0);
+    list.Append(i, i + 1);
+    EXPECT_EQ(list.At(i), i + 1);
+  }
+}
+
+TEST(List, Iterator) {
+  List<T> list;
+  for (auto i = 0; i < 1024; i += 1) list.Append(i, i + 1);
+  auto iter = list.Begin();
+  while (!iter->IsEnd()) {
+    EXPECT_EQ(iter->Index() + 1.0f, iter->Data());
+    iter = iter->Next();
+  }
+}
+
+T transform(const T &x) { return x + 1; }
+
+TEST(List, Map) {
+  List<T> list;
+  for (auto i = 0; i < 1024; i += 1) list.Append(i, i + 1);
+  auto new_list = list.Map(transform);
+  for (auto i = 0; i < 1024; i += 1) EXPECT_EQ(new_list->At(i), i + 2);
+}
+
+List<T>::ReduceStruct reduce(const List<T>::ReduceStruct &x,
+                             const List<T>::ReduceStruct &y) {
+  if (x.second < y.second) return x;
+  return y;
+}
+
+TEST(List, Reduce) {
+  List<T> list;
+  for (auto i = 0; i < 1024; i += 1) list.Append(i, i + 1);
+  auto result = list.Reduce(reduce, {-1, 100000000.0f});
+  EXPECT_EQ(result.first, 0);
+  EXPECT_EQ(result.second, 1.0f);
+}
+
+TEST(List, Add) {
+  List<T> list1, list2;
+  for (auto i = 0; i < 1024; i += 1) {
+    EXPECT_EQ(list1.At(2 * i), 0);
+    list1.Append(2 * i, i + 1);
+    EXPECT_EQ(list1.At(2 * i), i + 1);
+
+    EXPECT_EQ(list2.At(2 * i + 1), 0);
+    list2.Append(2 * i + 1, i + 1);
+    EXPECT_EQ(list2.At(2 * i + 1), i + 1);
+  }
+  list1.Add(&list2);
+  for (auto i = 0; i < 1024; i++) {
+    EXPECT_EQ(list1.At(2 * i), i + 1);
+    EXPECT_EQ(list1.At(2 * i + 1), i + 1);
+  }
+}
+
+TEST(List, Product) {
+  List<T> list1, list2;
+  for (auto i = 0; i < 1024; i += 1) {
+    list1.Append(2 * i, i + 1);
+
+    list2.Append(2 * i + 1, i + 1);
+  }
+  EXPECT_EQ(list1.Dot(&list2), 0.0f);
+}
+
+TEST(List, Produc2) {
+  List<T> list1, list2;
+  for (auto i = 0; i < 1024; i += 1) {
+    list1.Append(2 * i, 1);
+
+    list2.Append(2 * i, 1);
+  }
+  EXPECT_EQ(list1.Dot(&list2), 1024.0f);
+}
+
+TEST(List, Cross) {
+  List<T> list1, list2;
+  for (auto i = 0; i < 16; i += 1) {
+    list1.Append(i, i);
+    list2.Append(i, i);
+  }
+  Tableau<T> *result = list1.Cross(&list2, 16, 16);
+  for (auto i = 0; i < 16; i++) {
+    for (auto j = 0; j < 16; j++) {
+      EXPECT_EQ(result->At(i, j), i * j);
+    }
+  }
+}
+
+TEST(Tableau, Add) {
+  List<T> list1, list2;
+  for (auto i = 0; i < 16; i += 1) {
+    list1.Append(i, 1);
+    list2.Append(i, i);
+  }
+  Tableau<T> *tableau1 = list1.Cross(&list2, 16, 16);
+  Tableau<T> *tableau2 = list1.Cross(&list1, 16, 16);
+  tableau1->Add(tableau2);
+  for (auto i = 0; i < 16; i++) {
+    for (auto j = 0; j < 16; j++) {
+      EXPECT_EQ(tableau1->At(i, j), j + 1);
+    }
+  }
+}
